d204b412c8c7cb4408dbf77ce0a437969de77094
hossein <hosseinghafari4@gmail.com>
2024-05-15 09:31:25 +0330

    comit 2-030226

--- Industrial.Client/obj/Debug/net8.0/Industrial.Client.AssemblyInfo.cs
+++ Industrial.Client/obj/Debug/net8.0/Industrial.Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Industrial.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8fa877c44a185a53a967df2d2aef9d1288a95d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+807cfc3a3debcdbf930942e4268138f460cdf5ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("Industrial.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Industrial.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
