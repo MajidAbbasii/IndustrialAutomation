022207d8f4546ca106301b89d2daedd6ff2e3cfa
Majid Abbasi <m.abbasi201@gmail.com>
2024-05-24 16:34:58 +0430

    AddIdentityServerConfig

--- Industrial.BackEnd/Data/ApplicationContext.cs
+++ Industrial.BackEnd/Data/ApplicationContext.cs
@@ -1,24 +1,111 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Industrial.Domain.Entities;
+using Industrial.BackEnd.Models.Interfaces;
+using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
 using Industrial.BackEnd.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
-    public class ApplicationContext : DbContext
+namespace Industrial.BackEnd.Data
+{
+    public class ApplicationContext : IdentityDbContext<
+        User, Role, string,
+        IdentityUserClaim<string>,
+        UserRole,
+        IdentityUserLogin<string>,
+        IdentityRoleClaim<string>,
+        IdentityUserToken<string>
+    >
     {
-        public ApplicationContext (DbContextOptions<ApplicationContext> options)
-            : base(options)
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options, IHttpContextAccessor httpContextAccessor) : base(options)
         {
+            _httpContextAccessor = httpContextAccessor;
         }
 
-    public DbSet<Cart> Cart { get; set; } = default!;
-    public DbSet<Category> Category { get; set; } = default!;
-    public DbSet<Customer> Customer { get; set; } = default!;
-    public DbSet<Order> Order { get; set; } = default!;
-    public DbSet<OrderItem> OrderItem { get; set; } = default!;
-    public DbSet<Payment> Payment { get; set; } = default!;
-    public DbSet<Product> Product { get; set; } = default!;
-    public DbSet<Shipment> Shipment { get; set; } = default!;
+        public DbSet<Cart> Cart { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<OrderItem> OrderItem { get; set; }
+        public DbSet<Payment> Payment { get; set; }
+        public DbSet<Product> Product { get; set; }
+        public DbSet<Shipment> Shipment { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Rename AspNet default tables
+            builder.Entity<User>().ToTable("Users");
+            builder.Entity<Role>().ToTable("Roles");
+            builder.Entity<UserRole>().ToTable("UserRoles");
+            builder.Entity<IdentityUserClaim<string>>().ToTable("UserClaims");
+            builder.Entity<IdentityUserLogin<string>>().ToTable("UserLogins");
+            builder.Entity<IdentityRoleClaim<string>>().ToTable("RoleClaims");
+            builder.Entity<IdentityUserToken<string>>().ToTable("UserTokens");
+
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.UserRoles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
+
+            builder.Entity<User>()
+                .HasMany(e => e.UserRoles)
+                .WithOne()
+                .HasForeignKey(e => e.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+
+        }
+
+        /// <summary>
+        /// SaveChangesAsync with entities audit
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var entries = ChangeTracker
+              .Entries()
+              .Where(e => e.Entity is IAuditedEntityBase && (
+                  e.State == EntityState.Added
+                  || e.State == EntityState.Modified));
+
+            string modifiedOrCreatedBy = _httpContextAccessor?.HttpContext?.User?.Identity?.Name ?? "System";
+
+            foreach (var entityEntry in entries)
+            {
+                if (entityEntry.State == EntityState.Added)
+                {
+                    ((IAuditedEntityBase)entityEntry.Entity).CreatedDate = DateTime.Now;
+                    ((IAuditedEntityBase)entityEntry.Entity).CreatedBy = modifiedOrCreatedBy;
+                }
+                else
+                {
+                    Entry((IAuditedEntityBase)entityEntry.Entity).Property(p => p.CreatedDate).IsModified = false;
+                    Entry((IAuditedEntityBase)entityEntry.Entity).Property(p => p.CreatedBy).IsModified = false;
+                }
+              ((IAuditedEntityBase)entityEntry.Entity).LastModifiedDate = DateTime.Now;
+                ((IAuditedEntityBase)entityEntry.Entity).LastModifiedBy = modifiedOrCreatedBy;
+            }
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+    }
 }
--- Industrial.BackEnd/Program.cs
+++ Industrial.BackEnd/Program.cs
@@ -1,9 +1,24 @@
+using Industrial.BackEnd.Data;
+using Industrial.Domain.Entities;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationContext") ?? throw new InvalidOperationException("Connection string 'ApplicationContext' not found.")));
 
+builder.Services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>()
+                .AddDefaultTokenProviders();
+
+builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<User>()
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients)
+    .AddDeveloperSigningCredential();
+                
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -21,7 +36,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllers();
